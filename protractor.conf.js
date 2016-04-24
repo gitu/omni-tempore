8dc614b9abbbdc7a02dbaeefdc2cc51f7697b821
Florian Schrag <florian@schrag.org>
2016-04-24 17:33:31 +0200

    add sauce labs

--- protractor.conf.js
+++ protractor.conf.js
@@ -41,3 +41,14 @@ exports.config = {
    */
   useAllAngular2AppRoots: true
 };
+
+
+if (process.env.TRAVIS) {
+  exports.config.sauceUser = process.env.SAUCE_USERNAME;
+  exports.config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+  exports.config.capabilities = {
+    'browserName': 'chrome',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER
+  };
+}
